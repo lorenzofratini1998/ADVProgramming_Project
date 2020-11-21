47760d5fd651721383348bf2dfeecad8d3597c6b
andrea <andreapincia@gmail.com>
2020-11-21 17:30:22 +0100

    Entities archive e tag

--- src/main/java/it/univpm/advprog/blog/model/entities/Archive.java
+++ src/main/java/it/univpm/advprog/blog/model/entities/Archive.java
@@ -1,5 +1,51 @@
 package it.univpm.advprog.blog.model.entities;
 
-public class Archive {
-    //TODO
-}
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+	@Entity
+	@Table (name="archives")
+	public class Archive implements Serializable {
+	    
+		private String name;
+		private Set<Post> posts = new HashSet<Post>();
+	
+	
+	/** Getter per la proprieta' name dell'archivio
+	 * 
+	 * return name dell'archivio
+	 */
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="name",nullable=false, length= 20)
+	public String getName() {
+		return name;
+	}
+	
+	/** Setter per la proprieta' name dell'archivio
+	 * 
+	 * @param name del name dell'archivio
+	 * 
+	 */
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	/**
+	 * relazione uno a molti tra archives e posts
+	 * @return
+	 */
+	
+	@OneToMany(mappedBy="archive")
+	public Set<Post> getPosts(){
+		return posts;
+	}
+	
+	public void setPosts(Set<Post> posts) {
+		this.posts=posts;
+	}
+	
+	}
--- src/main/java/it/univpm/advprog/blog/model/entities/Tag.java
+++ src/main/java/it/univpm/advprog/blog/model/entities/Tag.java
@@ -1,7 +1,56 @@
 package it.univpm.advprog.blog.model.entities;
+	
+import javax.persistence.*;
+	
+import antlr.collections.List;
+	
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Tag {
-    //TODO
-    public void addPost(Post post) {
-    }
+	
+	@Entity
+	@Table(name="tags")
+	public class Tag implements Serializable {
+		
+		private String name;
+		private Set<Post> posts = new HashSet<Post>();
+		
+		/**
+		 * Getter per la proprieta' name del tag
+		 * 
+		 * return name del tag
+		 */
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name="name",nullable=false, length=20)
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	* Setter per la proprieta' name del tag
+	* @param name del name del tag
+	*/
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	/**
+	 * Vista dall'esempio di spegni tra singer e instrument
+	 * @return
+	 */
+	
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST, mappedBy="tags")
+	public Set<Post> getPosts(){
+		return posts;
+	}
+	
+	public void setPosts(Set<Post> posts) {
+		this.posts=posts;
+	}
+	
+	void addPost(Post post) {
+	}
 }
