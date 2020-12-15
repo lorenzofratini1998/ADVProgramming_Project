8d8f86b55c5ae516a919747971d4df95258c42dc
emanueleincicco <48356595+emanueleincicco@users.noreply.github.com>
2020-12-15 15:52:53 +0100

    fix error "No Session"

--- src/main/java/it/univpm/advprog/blog/model/entities/User.java
+++ src/main/java/it/univpm/advprog/blog/model/entities/User.java
@@ -4,16 +4,7 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
@@ -156,7 +147,7 @@ public class User implements Serializable {
 	 * @return commenti dell'utente
 	 */
 	
-	@OneToMany(mappedBy = "author", orphanRemoval = true)
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "author", orphanRemoval = true)
 	public Set<Comment> getComments() {
 		return this.comments;
 	}
@@ -190,7 +181,7 @@ public class User implements Serializable {
 	 * @return posts dell'utente
 	 */	
 	
-	@OneToMany(mappedBy = "author", orphanRemoval = true)
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "author", orphanRemoval = true)
 	public Set<Post> getPosts() {
 		return this.posts;
 	}
